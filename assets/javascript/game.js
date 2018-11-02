f5d5edb21b335eaf9568f9fa2c1dd9e6c4e0995c
ZGermain <zack.germain@gmail.com>
2018-11-01 19:59:34 -0500

    completed function to display choice buttons from question object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@
 var questions = [{
     question: "What is 2*5?",
     choices: [2, 5, 10, 15],
-    correctAnswer: 2
+    correctChoice: 2
   }, {
     question: "What is 3*6?",
     choices: [3, 6, 9, 18],
@@ -12,38 +12,46 @@ var questions = [{
   }, {
     question: "What is 8*9?",
     choices: [72, 99, 108, 134],
-    correctAnswer: 0
+    correctChoice: 0
   }, {
     question: "What is 1*7?",
     choices: [4, 5, 6, 7],
-    correctAnswer: 3
+    correctChoice: 3
   }, {
     question: "What is 8*8?",
     choices: [20, 30, 40, 64],
-    correctAnswer: 3
+    correctChoice: 3
   }];
 
 correctAnswers = 0;
 incorrectAnswers = 0;
 secondsLeft = 0;
 gameOver = true;
+    //counter for question turns
 questionNumber = 0;
 
 //Essential Functions
 
     //Function to set a Question timer
-setTimeout();
+// setTimeout();
 
     //Function to set an invisible "wait a few seconds" timer
 
     //Function to clear the "last page" jQuery .empty()
 
     //Function to display questions and buttons
-    function displayButtons(questionNumber) {
-    for (i=0; i < questions[questionNumber].correctAnswer[i]; i++){
+    function displayButtons() {
 
+    for (i=0; i < questions[questionNumber].choices.length; i++){
+        var newButton = $("<button>");
+        var newDiv = $("<div>")
+        newButton.text(questions[questionNumber].choices[i]);
+        newButton.attr("value", i);
+        newDiv.append(newButton);
+        $(".container").append(newDiv);
     }
 };
+displayButtons();
 
     //Function to display right/wrong answer page 
 
@@ -62,6 +70,8 @@ setTimeout();
     //Function to show right/wrong answer result
         //Clear Divs from Last Screen
         //Display "You are Right" or "You are a Looooser"
+
+    //Function to show End Game result
 
 
 
