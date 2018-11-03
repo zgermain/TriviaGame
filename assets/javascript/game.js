9bf7a86b4ec71c7fca5565262e547272f6f97449
ZGermain <zack.germain@gmail.com>
2018-11-03 15:24:39 -0500

    replaced placeholder trivia with comic trivia. added style and google fonts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,45 @@
 
 //Global Variables
 var questions = [{
-    question: "What is my name?",
-    choices: ["Zack", "Josh", "Scooby-Dooby-Doo", "Sarah"],
-    correctChoice: "Zack"
+    question: "What superhero was born with the Curse of Kordax?",
+    choices: ["Captain Marvel", "Hawkman", "Ghost Rider", "Aquaman"],
+    correctChoice: "Aquaman"
   }, {
-    question: "Where are you",
-    choices: ["Cincinnati", "Chicago", "Washington D.C.", "Evergreen"],
-    correctChoice: "Chicago"
+    question: "What super villain was created when Emil Blonsky accidentally exposed himself to a concentrated burst of gamma rays?",
+    choices: ["Abomination", "Doc Samson", "The Leader", "Bi-Beast"],
+    correctChoice: "Abomination"
   }, {
-    question: "What is....your favorite color?",
-    choices: ["Purple", "Yellow", "Red...I mean Blue!", "Aqua"],
-    correctChoice: "Red...I mean Blue!"
+    question: "What Chester Gould character made his first appearance in the Detroit Free Press on October 4, 1931?",
+    choices: ["Buck Rogers", "Dick Tracy", "Superman", "Flash Gordon"],
+    correctChoice: "Dick Tracy"
+  }, {
+    question: "What superhero grew up on the planet Tamaran?",
+    choices: ["Hawkgirl", "Ultra Boy", "The Silver Surfer", "Starfire"],
+    correctChoice: "Starfire"
+  }, {
+    question: "Edwin Jarvis serves as butler to what superteam?",
+    choices: ["Fantastic Four", "Avengers", "Justice League", "Teen Titans"],
+    correctChoice: "Avengers"
+  }, {
+    question: "Which of these superheroines was inhabited by the soul of an ancient Egyptian princess after attempting suicide?",
+    choices: ["Black Canary", "Wonder Woman", "Huntress", "Hawkgirl"],
+    correctChoice: "Hawkgirl"
+  }, {
+    question: "Who was the first costumed superhero?",
+    choices: ["Superman", "Arrow", "Crimson Avenger", "The Phantom"],
+    correctChoice: "The Phantom"
+  }, {
+    question: "Who is Luke Cage's best friend and longtime partner in crime-fighting?",
+    choices: ["Human Torch", "Doctor Strange", "Daredevil", "Iron Fist"],
+    correctChoice: "Iron Fist"
+  }, {
+    question: "What superhero gained his powers after being exposed to the Orb of Ra?",
+    choices: ["Hawkman", "Doctor Strange", "Metamorpho", "Iron Fist"],
+    correctChoice: "Metamorpho"
+  }, {
+    question: "Who is Skaar?",
+    choices: ["Son of Tarzan", "Son of Wolverine", "Son of Hulk", "Son of Thor"],
+    correctChoice: "Son of Hulk"
   }
 ];
 
@@ -43,7 +71,7 @@ function decrement() {
 
     countDownTimer--;
 
-    $("#timer").html("<h3>" + countDownTimer + "</h3>");
+    $("#timer").html("<h3>Time Left: " + countDownTimer + "</h3>");
     if (countDownTimer === 0) {
       stopTimer();
       unansweredScore++;
@@ -55,19 +83,21 @@ function decrement() {
 function displayQuestion() {
     $("#question-div").empty();
     countDownTimer = 10;
-    $("#timer").html("<h3>" + countDownTimer + "</h3>");
+    $("#timer").html("<h3>Time Left: " + countDownTimer + "</h3>");
     runTimer();
 
+    //Fills in new question into a div
     var newDiv = $("<div>")
     newDiv.append(`<p>${questions[questionNumber].question}</p>`);
     $("#question-div").append(newDiv);
 
+    //Creates buttons based on current question
     for (i=0; i < questions[questionNumber].choices.length; i++){
         var newButton = $("<button>");
         var newDiv = $("<div>")
         newButton.text(questions[questionNumber].choices[i]);
         newButton.attr("value", questions[questionNumber].choices[i]);
-        newButton.attr("class", "choice-button");
+        newButton.attr("class", "choice-button rounded");
         newDiv.append(newButton);
         $("#question-div").append(newDiv);
     }
@@ -112,7 +142,7 @@ function displayResult(){
         delayDisplay();
     } else {
         $("#question-div").append(`<h2>LOOOOSER!!!</h2>`);
-        $("#question-div").append(`<p>The correct answer was ${questions[questionNumber].correctChoice}</p>`);
+        $("#question-div").append(`<p>The correct answer was <strong>${questions[questionNumber].correctChoice}</strong></p>`);
         delayDisplay();
     };
     
