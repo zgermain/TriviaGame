a0aac5d7d80985b39b20db8bf6a58b8ffe411819
ZGermain <zack.germain@gmail.com>
2018-11-03 21:37:12 -0500

    added photos for each answer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,43 +4,53 @@
 var questions = [{
     question: "What superhero was born with the Curse of Kordax?",
     choices: ["Captain Marvel", "Hawkman", "Ghost Rider", "Aquaman"],
-    correctChoice: "Aquaman"
+    correctChoice: "Aquaman",
+    img: "<img src='assets/images/Aquaman.jfif'>"
   }, {
     question: "What villain was created when Emil Blonsky accidentally exposed himself to a burst of gamma rays?",
     choices: ["Abomination", "Doc Samson", "The Leader", "Bi-Beast"],
-    correctChoice: "Abomination"
+    correctChoice: "Abomination",
+    img: "<img src='assets/images/Abomination.jpg'>"
   }, {
     question: "What Chester Gould character made his first appearance in the Detroit Free Press on October 4, 1931?",
     choices: ["Buck Rogers", "Dick Tracy", "Superman", "Flash Gordon"],
-    correctChoice: "Dick Tracy"
+    correctChoice: "Dick Tracy",
+    img: "<img src='assets/images/Dick Tracy.jpg'>"
   }, {
     question: "What superhero grew up on the planet Tamaran?",
     choices: ["Hawkgirl", "Ultra Boy", "The Silver Surfer", "Starfire"],
-    correctChoice: "Starfire"
+    correctChoice: "Starfire",
+    img: "<img src='assets/images/starfire.jpg'>"
   }, {
     question: "Edwin Jarvis serves as butler to what superteam?",
     choices: ["Fantastic Four", "Avengers", "Justice League", "Teen Titans"],
-    correctChoice: "Avengers"
+    correctChoice: "Avengers",
+    img: "<img src='assets/images/Avengers.jpg'>"
   }, {
     question: "Which of these superheroines was inhabited by the soul of an ancient Egyptian princess?",
     choices: ["Black Canary", "Wonder Woman", "Huntress", "Hawkgirl"],
-    correctChoice: "Hawkgirl"
+    correctChoice: "Hawkgirl",
+    img: "<img src='assets/images/Hawkgirl.jpg'>"
   }, {
     question: "Who was the first costumed superhero?",
     choices: ["Superman", "Arrow", "Crimson Avenger", "The Phantom"],
-    correctChoice: "The Phantom"
+    correctChoice: "The Phantom",
+    img: "<img src='assets/images/Phantom.jpeg'>"
   }, {
     question: "Who is Luke Cage's best friend and longtime partner in crime-fighting?",
     choices: ["Human Torch", "Doctor Strange", "Daredevil", "Iron Fist"],
-    correctChoice: "Iron Fist"
+    correctChoice: "Iron Fist",
+    img: "<img src='assets/images/Iron Fist.jpg'>"
   }, {
     question: "What superhero gained his powers after being exposed to the Orb of Ra?",
     choices: ["Hawkman", "Doctor Strange", "Metamorpho", "Iron Fist"],
-    correctChoice: "Metamorpho"
+    correctChoice: "Metamorpho",
+    img: "<img src='assets/images/Metamorpho.jpg'>"
   }, {
     question: "Who is Skaar?",
     choices: ["Son of Tarzan", "Son of Wolverine", "Son of Hulk", "Son of Thor"],
-    correctChoice: "Son of Hulk"
+    correctChoice: "Son of Hulk",
+    img: "<img src='assets/images/Skaar.jpg'>"
   }
 ];
 
@@ -149,20 +159,24 @@ function choicePress() {
 function displayResult(){
 
     $("#question-div").empty();
+ 
 
     if (countDownTimer === 0){
         $("#question-div").append(`<h2>You ran out of Time!!!</h2>`);
         $("#question-div").append(`<p>The correct answer was ${questions[questionNumber].correctChoice}</p>`);
         responsiveVoice.speak("You ran out of Time! The correct answer was "+questions[questionNumber].correctChoice, "UK English Male");
+        $("#question-div").append(`${questions[questionNumber].img}`);
         delayDisplay();
     } else if (userCorrect === true){
         $("#question-div").append(`<h2>You Got It!</h2>`);
         responsiveVoice.speak("You got it!", "UK English Male");
-        //display picture
+        $("#question-div").append(`${questions[questionNumber].img}`);
         delayDisplay();
     } else {
         $("#question-div").append(`<h2>Wrong!!!</h2>`);
         $("#question-div").append(`<p>The correct answer was <strong>${questions[questionNumber].correctChoice}</strong></p>`);
+        $("#question-div").append(`${questions[questionNumber].img}`);
+
         responsiveVoice.speak("Wrong! The correct answer was "+questions[questionNumber].correctChoice, "UK English Male");
         delayDisplay();
     };
